a7e0837286c53b065da39ceb85bb63611448f7d0
AdibAhmed317 <ahmedadib317@gmail.com>
2023-12-18 13:35:52 +0600

    auth page

--- client/app/(auth)/layout.tsx
+++ client/app/(auth)/layout.tsx
@@ -3,10 +3,10 @@ import { Navbar } from '../../components/navbar';
 
 const AuthLayout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className='h-full bg-slate-100'>
+    <div className='h-full bg-slate-50'>
       <Navbar />
-      <main className='py-20 bg-slate-100 h-full w-full flex justify-center items-center'>
-        <div className='p-10 bg-slate-200 rounded-md'>{children}</div>
+      <main className='py-20 h-full w-full flex justify-center items-center'>
+        <div className='p-10 bg-slate-200 rounded-md shadow-lg'>{children}</div>
       </main>
       <Footer />
     </div>
--- client/components/forms/signupform.tsx
+++ client/components/forms/signupform.tsx
@@ -1,7 +1,125 @@
+'use client';
+
 import React from 'react';
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '../ui/form';
+import { useForm } from 'react-hook-form';
+import * as z from 'zod';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { Button } from '../ui/button';
+import { Input } from '../ui/input';
+import Link from 'next/link';
+
+const FormSchema = z
+  .object({
+    email: z.string().min(1, 'Email is required.').email('Invalid email'),
+    password: z
+      .string()
+      .min(1, 'Password is required.')
+      .min(8, 'Password at least contain 8 characters'),
+    name: z.string().min(1, 'Name cannot be empty'),
+    confirmPassword: z.string().min(1, 'Password did not matched.'),
+  })
+  .refine((data) => data.password === data.confirmPassword, {
+    path: ['confirmPassword'],
+  });
 
 const SignUpForm = () => {
-  return <div>SignUpForm</div>;
+  const form = useForm<z.infer<typeof FormSchema>>({
+    resolver: zodResolver(FormSchema),
+  });
+
+  const onSubmit = (values: z.infer<typeof FormSchema>) => {
+    console.log(values);
+  };
+
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className='w-full'>
+        <div className='space-y-3'>
+          <FormField
+            control={form.control}
+            name='name'
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Name</FormLabel>
+                <FormControl>
+                  <Input placeholder='Enter your name' {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name='email'
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Email</FormLabel>
+                <FormControl>
+                  <Input placeholder='mail@example.com' {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name='password'
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Password</FormLabel>
+                <FormControl>
+                  <Input
+                    placeholder='Enter your password'
+                    type='password'
+                    {...field}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name='confirmPassword'
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Confirm Password</FormLabel>
+                <FormControl>
+                  <Input
+                    placeholder='Re-enter your password'
+                    type='password'
+                    {...field}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+        </div>
+
+        <Button className='w-full bg-black/70 mt-10' type='submit'>
+          Create Account
+        </Button>
+      </form>
+      <div className='mx-auto my-4 flex w-full items-center justify-evenly before:mr-4 before:black before:h-px before:flex-grow before:bg-stone-400 after:ml-4 after:block after:h-px after:flex-grow after:bg-stone-400'>
+        or
+      </div>
+      <p className='text-center text-sm text-gray-600 mt-2'>
+        Already have an account? please&nbsp;
+        <Link className='text-blue-500 hover:underline' href='/sign-in'>
+          Sign in
+        </Link>
+      </p>
+    </Form>
+  );
 };
 
 export default SignUpForm;
