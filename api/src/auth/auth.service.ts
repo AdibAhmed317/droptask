b6d544064d3f099ae264e99d7ff7ad637665e2b4
AdibAhmed317 <ahmedadib317@gmail.com>
2023-12-22 20:47:26 +0600

    login jwt token (api)

--- api/src/auth/auth.service.ts
+++ api/src/auth/auth.service.ts
@@ -1,7 +1,44 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { LoginDto } from './dto/auth.dto';
+import { UserService } from 'src/user/user.service';
+import { compare } from 'bcrypt';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
-  async login(loginDto: LoginDto) {}
+  constructor(
+    private userService: UserService,
+    private jwtService: JwtService,
+  ) {}
+
+  async login(loginDto: LoginDto) {
+    const user = await this.validateUser(loginDto);
+
+    const payload = {
+      email: user.email,
+      sub: {
+        name: user.name,
+      },
+    };
+
+    return {
+      user,
+      backendTokens: {
+        accessToken: await this.jwtService.signAsync(payload, {
+          expiresIn: '2d',
+        }),
+      },
+    };
+  }
+
+  async validateUser(loginDto: LoginDto) {
+    const user = await this.userService.findByEmail(loginDto.email);
+
+    if (user && (await compare(loginDto.password, user.password))) {
+      const { password, ...result } = user;
+      return result;
+    } else {
+      throw new UnauthorizedException('Invalid Email/Password.');
+    }
+  }
 }
--- api/src/user/user.service.ts
+++ api/src/user/user.service.ts
@@ -21,6 +21,12 @@ export class UserService {
       },
     });
 
-    return newUser;
+    const { password, ...result } = newUser;
+
+    return result;
+  }
+
+  async findByEmail(email: string) {
+    return await this.prisma.user.findUnique({ where: { email: email } });
   }
 }
