cab9e688ae2d6b68e857f1c61974ca25483c52d3
AdibAhmed317 <ahmedadib317@gmail.com>
2024-03-09 03:18:14 +0600

    dnd (client)

--- apps/client/components/dashboard/TeamList.tsx
+++ apps/client/components/dashboard/TeamList.tsx
@@ -1,52 +1,52 @@
-import { cn } from '@/lib/utils';
-import { Roboto } from 'next/font/google';
-import Link from 'next/link';
-import React from 'react';
+import { cn } from "@/lib/utils";
+import { Roboto } from "next/font/google";
+import Link from "next/link";
+import React from "react";
 
 const textFont = Roboto({
-  subsets: ['latin'],
-  weight: ['300'],
+  subsets: ["latin"],
+  weight: ["300"],
 });
 
 const TeamList = () => {
   return (
-    <div className='grid grid-cols-2 md:grid-cols-5 gap-2'>
-      <section className='container mt-10'>
-        <Link href='/dashboard/team/1/kanban'>
-          <div className='max-w-[15rem] bg-gray-50 shadow-lg rounded-lg overflow-hidden'>
-            <div className='flex items-center justify-center bg-gray-500 h-24'>
-              <h2 className='text-white text-4xl font-semibold'>DT</h2>
+    <div className="grid grid-cols-2 md:grid-cols-5 gap-2">
+      <section className="container mt-10">
+        <Link href="/dashboard/team/1/kanban">
+          <div className="max-w-[15rem] bg-gray-50 hover:bg-gray-200 transition-all shadow-lg rounded-lg overflow-hidden">
+            <div className="flex items-center justify-center bg-gray-500 h-24">
+              <h2 className="text-white text-4xl font-semibold">DT</h2>
             </div>
-            <div className='p-4 flex justify-center items-center'>
-              <h2 className={cn('text-xl mb-2', textFont.className)}>
+            <div className="p-4 flex justify-center items-center">
+              <h2 className={cn("text-xl mb-2", textFont.className)}>
                 Design Team
               </h2>
             </div>
           </div>
         </Link>
       </section>
-      <section className='container mt-10'>
-        <Link href='/dashboard/team/1/kanban'>
-          <div className='max-w-[15rem] bg-gray-50 shadow-lg rounded-lg overflow-hidden'>
-            <div className='flex items-center justify-center bg-gray-500 h-24'>
-              <h2 className='text-white text-4xl font-semibold'>DT</h2>
+      <section className="container mt-10">
+        <Link href="/dashboard/team/1/kanban">
+          <div className="max-w-[15rem] bg-gray-50 hover:bg-gray-200 transition-all shadow-lg rounded-lg overflow-hidden">
+            <div className="flex items-center justify-center bg-gray-500 h-24">
+              <h2 className="text-white text-4xl font-semibold">DT</h2>
             </div>
-            <div className='p-4 flex justify-center items-center'>
-              <h2 className={cn('text-xl mb-2', textFont.className)}>
+            <div className="p-4 flex justify-center items-center">
+              <h2 className={cn("text-xl mb-2", textFont.className)}>
                 Development Team
               </h2>
             </div>
           </div>
         </Link>
       </section>
-      <section className='container mt-10'>
-        <Link href='/dashboard/team/1/kanban'>
-          <div className='max-w-[15rem] bg-gray-50 shadow-lg rounded-lg overflow-hidden'>
-            <div className='flex items-center justify-center bg-gray-500 h-24'>
-              <h2 className='text-white text-4xl font-semibold'>MT</h2>
+      <section className="container mt-10">
+        <Link href="/dashboard/team/1/kanban">
+          <div className="max-w-[15rem] bg-gray-50 hover:bg-gray-200 transition-all shadow-lg rounded-lg overflow-hidden">
+            <div className="flex items-center justify-center bg-gray-500 h-24">
+              <h2 className="text-white text-4xl font-semibold">MT</h2>
             </div>
-            <div className='p-4 flex justify-center items-center'>
-              <h2 className={cn('text-xl mb-2', textFont.className)}>
+            <div className="p-4 flex justify-center items-center">
+              <h2 className={cn("text-xl mb-2", textFont.className)}>
                 Marketing Team
               </h2>
             </div>
--- apps/client/components/team/kanban/KanbanBoard.tsx
+++ apps/client/components/team/kanban/KanbanBoard.tsx
@@ -1,17 +1,20 @@
-'use client';
+"use client";
 
-import { Button } from '@/components/ui/button';
-import { Column, Id } from '@/lib/types';
-import React, { useMemo, useState } from 'react';
-import { v4 as uuidv4 } from 'uuid';
-import ColumnContainer from './ColumnContainer';
-import { PlusCircle } from 'lucide-react';
-import { DndContext } from '@dnd-kit/core';
-import { SortableContext } from '@dnd-kit/sortable';
+import { Button } from "@/components/ui/button";
+import { Column, Id } from "@/lib/types";
+import React, { useMemo, useState } from "react";
+import { v4 as uuidv4 } from "uuid";
+import ColumnContainer from "./ColumnContainer";
+import { PlusCircle } from "lucide-react";
+import { DndContext, DragOverlay, DragStartEvent } from "@dnd-kit/core";
+import { SortableContext } from "@dnd-kit/sortable";
+import { createPortal } from "react-dom";
 
 const KanbanBoard = () => {
   const [columns, setColumns] = useState<Column[]>([]);
   const columnsId = useMemo(() => columns.map((col) => col.id), [columns]);
+
+  const [activeColumn, setActiveColumn] = useState<Column | null>(null);
 
   const createColumn = () => {
     const columnToAdd: Column = {
@@ -22,17 +25,25 @@ const KanbanBoard = () => {
   };
 
   const deleteColumn = (id: Id) => {
-    console.log('called');
+    console.log("called");
 
     const filteredColumns = columns.filter((col) => col.id !== id);
     setColumns(filteredColumns);
   };
 
+  const onDragStart = (event: DragStartEvent) => {
+    console.log("drag start");
+    if (event.active.data.current?.type === "Column") {
+      setActiveColumn(event.active.data.current.column);
+      return;
+    }
+  };
+
   return (
-    <div className='m-auto min-h-screen w-full overflow-x-auto overflow-y-hidden px-[40px]'>
-      <DndContext>
-        <div className='m-auto flex gap-4'>
-          <div className='flex gap-4'>
+    <div className="m-auto min-h-screen w-full overflow-x-auto overflow-y-hidden px-[40px]">
+      <DndContext onDragStart={onDragStart}>
+        <div className="m-auto flex gap-4">
+          <div className="flex gap-4">
             <SortableContext items={columnsId}>
               {columns.map((col) => (
                 <ColumnContainer
@@ -44,12 +55,24 @@ const KanbanBoard = () => {
             </SortableContext>
           </div>
           <Button
-            className='h-6 text-xs bg-gray-300 text-black/75 hover:text-white/95 flex gap-2'
-            onClick={createColumn}>
-            <PlusCircle className='max-h-4' />
+            className="h-6 text-xs bg-gray-300 text-black/75 hover:text-white/95 flex gap-2"
+            onClick={createColumn}
+          >
+            <PlusCircle className="max-h-4" />
             Add column
           </Button>
         </div>
+        {createPortal(
+          <DragOverlay>
+            {activeColumn && (
+              <ColumnContainer
+                column={activeColumn}
+                deleteColumn={deleteColumn}
+              />
+            )}
+          </DragOverlay>,
+          document.body
+        )}
       </DndContext>
     </div>
   );
